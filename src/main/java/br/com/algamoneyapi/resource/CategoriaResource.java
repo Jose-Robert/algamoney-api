7e878d5bf25cc9569fb0603e33131af3124081a3
José Robert <robertspfc10@gmail.com>
2020-05-01 11:32:36 -0300

    Desafio: Retornar 404 caso não exista a categoria

--- src/main/java/br/com/algamoneyapi/resource/CategoriaResource.java
+++ src/main/java/br/com/algamoneyapi/resource/CategoriaResource.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,6 +40,11 @@ public class CategoriaResource {
 		
 		return ResponseEntity.created(uri).body(categoriaSalva);
 		
+	}
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo ){
+		Categoria categoria = categoriaRepository.findOne(codigo);
+		return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
 	}
 
 }
