74af50c294efc55c65ab6519d1a1dd83d9408982
José Robert <robert.suporteweb@gmail.com>
2020-05-29 02:18:04 -0300

    adiciona Pesquisa de pessoa

--- src/main/java/br/com/algamoney/api/repository/PessoaRespository.java
+++ src/main/java/br/com/algamoney/api/repository/PessoaRespository.java
@@ -1,5 +1,7 @@
 package br.com.algamoney.api.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import br.com.algamoney.api.model.Pessoa;
 @Repository
 public interface PessoaRespository extends JpaRepository<Pessoa, Long>{
 
+	public Page<Pessoa> findByNomeContaining(String nome, Pageable pageable);
 }
